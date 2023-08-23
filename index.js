7f09a4b0e4638eed890b9221322690738c4732f5
L.J Solis <l.jsolis52@yahoo.com>
2023-08-22 20:16:13 -0500

    Finally added the right file structure and everything is working. I also added the packages needed for the assignment.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+
+// Inquirer import
 const inquirer = require('inquirer');
+//File system import
+const fs = require('fs');
+// Allow us to import classes from the shapes.js file in the directory
+const { Square, Triangle, Circle } = require('./lib/shapes.js')
+
+
