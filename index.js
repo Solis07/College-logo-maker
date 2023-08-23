dedc3068d2ecf3d472b212fdaddf908fa2f2e813
L.J Solis <l.jsolis52@yahoo.com>
2023-08-22 21:19:00 -0500

    Created a square test that will be used once I finishthe index.js in order to check if the shape renders with the correct color input.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Inquirer import
 const inquirer = require("inquirer");
-//File system import
+// File system import
 const fs = require("fs");
 // Allow us to import classes from the shapes.js file in the directory
 const { Square, Triangle, Circle } = require("./lib/shapes.js");
