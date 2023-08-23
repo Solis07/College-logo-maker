0c7a3c353763607e59c166c624a045ad039faa07
L.J Solis <l.jsolis52@yahoo.com>
2023-08-22 20:37:20 -0500

    added an array of questions that will utilize inquirer that will prompt the user with questions through the command line about the shape, shape color, text, and text color they would like.

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-
 // Inquirer import
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
 //File system import
-const fs = require('fs');
+const fs = require("fs");
 // Allow us to import classes from the shapes.js file in the directory
-const { Square, Triangle, Circle } = require('./lib/shapes.js')
+const { Square, Triangle, Circle } = require("./lib/shapes.js");
 
+// Utilizes inquirer to ask the user a set of questions about the shape, shape color, text and text color through the command line.
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        // Shape
+        type: "list",
+        name: "shape",
+        message: "Choose a shape:",
+        choices: ["Square", "Triangle", "Circle"],
+      },
 
+      {
+        // Shape color
+        type: "input",
+        name: "shapeColor",
+        message: "Choose a color (Keyword or Hexadecimal):",
+      },
+
+      {
+        // Text
+        type: "input",
+        name: "text",
+        message: "Display a text (3 characters or less):",
+      },
+
+      {
+        // Text color
+        type: "input",
+        name: "textColor",
+        message: "Choose a text color (Keyword or Hexadecimal):",
+      },
+    ])
+    .then((answers) => {
+      if (answers.text.length > 3) {
+        console.log("3 characters max");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
