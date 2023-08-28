f72681b0f062306898a485bfbe78efaf0d6cf7e6
L.J Solis <l.jsolis52@yahoo.com>
2023-08-27 22:07:01 -0500

    Realized it was adding the shape twice in the document. I also  needed  to delete a common on line 39. The index is finally working and I can finally finish the project. The shapes need a little modify to fit the text but other than that everything else works.

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ function promptUser() {
         type: "input",
         name: "textColor",
         message: "Choose a text color (Keyword OR Hexadecimal):",
-      },
+      }
     ])
-    .then((answers) => {
-      if (answers.text.length > 3) {
+    .then((choice) => {
+      if (choice.text.length > 3) {
         console.log("3 characters max");
         promptUser();
       } else {
-        writeToFile("logo.svg", answers);
+        writeToFile("logo.svg", choice);
       }
     });
 }
@@ -51,13 +51,37 @@ function promptUser() {
 // Calls the promptUser function
 promptUser();
 
+// Will write the SVG file after the user answers the questions
+function writeToFile(fileName, choice) {
+// Empty string  
+  let svgLogo = "";
+// Sets container width and height for the logo
+  svgLogo = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">`;
+// Inserts the user input into the svg file
+  svgLogo += `${choice.shape}`;
 
-// This will allow the user to take their input from the array and apply the properties of what shape they want.
-let shapeChoice;
-if (answer.shape === "Square") {
-  shapeChoice = new Square();
-} else if (answer.shape === "Triangle") {
-  shapeChoice = new Triangle();
-} else (answer.shape === "Circle"){
-  shapeChoice = new Circle();
+
+  // This will allow the user to take their input from the array and apply the properties of what shape and color they want.
+  let svgShape;
+  if (choice.shape === "Square") {
+    svgShape = new Square();
+  } else if (choice.shape === "Triangle") {
+    svgShape = new Triangle();
+  } else {
+    (choice.shape === "Circle")
+    svgShape = new Circle();
+  }
+   
+  svgShape.setColor(choice.shapeColor);
+  console.log(svgShape.render());
+
+// Set a default text size of 50 and displays the text and text color on the logo
+  svgLogo += svgShape.render();
+  svgLogo += `<text x="150" y="125" font-size="50" text-anchor="middle" fill="${choice.textColor}">${choice.text}</text>`;
+ 
+  svgLogo += '</svg>'
+
+  fs.writeFile(fileName, svgLogo, (err) => {
+    err ? console.log(err) : console.log("File created");
+  });
 }
